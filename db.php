cfa2e4f6b0b4d7ccf321ed1371d3e1ac52403b68
Darkness01222 <95456204+Darkness01222@users.noreply.github.com>
2024-05-20 15:34:36 -0600

    Create db.php

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+  $servername = "proyectofinadb.mysql.database.azure.com";
+  $username = "DBUser";
+  $password = "Darkness01222591837";
+  $dbname = "proyecto";
+  
+  // Crear conexión
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  
+  // Comprobar conexión
+  if ($conn->connect_error) {
+      die("Conexión fallida: " . $conn->connect_error);
+  }
+?>
